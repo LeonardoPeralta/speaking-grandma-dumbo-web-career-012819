97a7a35d21b75b5a85637c6bf109a1d894619811
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-15 18:16:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase == phrase.downcase
-    return 'HUH?! SPEAK UP, SONNY!'
+    return 'NO, NOT SINCE 1938!'
   else
-    return 'HUH?! SPEAK UP, SONNY!''NO, NOT SINCE 1938!'
+    return 'HUH?! SPEAK UP, SONNY!'
   end
 end
